d2a9d8cf71dd163990d897f5d601bb4b4da29ef0
Ifty64bit <ifty64bit@gmail.com>
2025-05-05 00:51:50 +0600

    add frog slider and sound effects

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,6 +5,7 @@ const rainSlider = document.querySelector("#rain-slider") as HTMLInputElement;
 const forestBirdSlider = document.querySelector(
     "#forest-bird-slider"
 ) as HTMLInputElement;
+const frogSlider = document.querySelector("#frog-slider") as HTMLInputElement;
 const powerButton = document.querySelector("#power") as HTMLButtonElement;
 
 const effects = {
@@ -14,6 +15,10 @@ const effects = {
     }),
     forestBird: new Howl({
         src: ["/assets/sounds/forest-bird.wav"],
+        loop: true,
+    }),
+    frog: new Howl({
+        src: ["/assets/sounds/frog.mp3"],
         loop: true,
     }),
 };
@@ -28,12 +33,21 @@ function handleForestBirdChange(e: Event) {
     effects.forestBird.volume(Number(value) / 100);
 }
 
+function handleFrogChange(e: Event) {
+    const value = (e.target as HTMLInputElement).value;
+    effects.frog.volume(Number(value) / 100);
+}
+
 rainSlider.addEventListener("input", (e) => {
     handleRainChange(e);
 });
 
 forestBirdSlider.addEventListener("input", (e) => {
     handleForestBirdChange(e);
+});
+
+frogSlider.addEventListener("input", (e) => {
+    handleFrogChange(e);
 });
 
 powerButton.addEventListener("click", () => {
